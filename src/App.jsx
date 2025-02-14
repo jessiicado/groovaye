0fa076a34ec6faace6ce149bfcf523d1de91c92c
Jessica Do <jessicanhu.do@gmail.com>
2025-02-13 23:36:41 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HIIIII!!!</h1>
+
+      {/* Header: Logo and hotbar */}
+      <NavBar />
+      {/* <Header /> */}
+      {/* 'Tinder' Cards  */}
+      {/* Buttons on bottom Tinder Cards  */}
+
+      {/* Discography/5 top songs of the artist */}
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Play.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Play() {
+  return (
+    <div className="Play">
+      <h1>LOLOL</h1>
+    </div>
+  );
+}
+
+export default Play;
